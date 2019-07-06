29b4f8c015b7fde7cf5ec05ebd0d4eea9037334d
Lenny Peters <mr.lennypeters@gmail.com>
2019-07-06 09:55:53 -0700

    testing again

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
-// const dotenv = require("dotenv");
-
-// const result = dotenv.config();
-// if (result.error) {
-//   throw result.error;
-// }
-// const { parsed: envs } = result;
-// console.log(envs);
-// module.exports = envs;
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/blanco/" : "/"
+};
