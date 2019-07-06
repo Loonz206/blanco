c0fbc28f7faa551f4a6546fe3078493c7e7a3fbb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-06 16:57:09 +0000

    Deploy Loonz206/blanco to github.com/Loonz206/blanco.git:gh-pages

---
+++ precache-manifest.10bc525eaa9083b49bfa722680c77f8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cacecb67bd64c71bd936",
+    "url": "/blanco/js/about.c2b5042a.js"
+  },
+  {
+    "revision": "a20dc53865d5c6e863ea",
+    "url": "/blanco/css/app.bb8d29fc.css"
+  },
+  {
+    "revision": "a20dc53865d5c6e863ea",
+    "url": "/blanco/js/app.4cb3375f.js"
+  },
+  {
+    "revision": "ce808654002c0146fe16",
+    "url": "/blanco/js/chunk-vendors.fe2ef355.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/blanco/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "04caa8ae54227cb3d5e5516d372cc30c",
+    "url": "/blanco/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blanco/robots.txt"
+  }
+];
