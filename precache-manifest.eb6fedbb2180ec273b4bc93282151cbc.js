8217faad4dc60cd554f874f07fbc13af2338aac2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-16 04:17:25 +0000

    Deploy Loonz206/blanco to github.com/Loonz206/blanco.git:gh-pages

---
+++ precache-manifest.eb6fedbb2180ec273b4bc93282151cbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2801aa249badd30c3b1c",
+    "url": "/blanco/js/about.9c7a9d5d.js"
+  },
+  {
+    "revision": "6346b61a4ee59bc25129",
+    "url": "/blanco/css/app.bb8d29fc.css"
+  },
+  {
+    "revision": "6346b61a4ee59bc25129",
+    "url": "/blanco/js/app.594ed094.js"
+  },
+  {
+    "revision": "8699643d6fff886e1fd8",
+    "url": "/blanco/css/chunk-00a8373c.9f252f94.css"
+  },
+  {
+    "revision": "8699643d6fff886e1fd8",
+    "url": "/blanco/js/chunk-00a8373c.46260eb2.js"
+  },
+  {
+    "revision": "ce808654002c0146fe16",
+    "url": "/blanco/js/chunk-vendors.fe2ef355.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/blanco/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "21124c8a6385a387d34b81db34d76cb6",
+    "url": "/blanco/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blanco/robots.txt"
+  }
+];
